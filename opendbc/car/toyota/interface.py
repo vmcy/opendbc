c20ea7977dd485f8a5a0d12d4f1048f9b0328105
vmcy <mahcyen@gmail.com>
2025-04-14 22:03:12 +0800

    try silent safetymodel

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -23,8 +23,8 @@ class CarInterface(CarInterfaceBase):
   @staticmethod
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
     ret.brand = "toyota"
-    ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.noOutput)]
-    ret.safetyConfigs[0].safetyParam = 1
+    ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.silent)]
+    ret.safetyConfigs[0].safetyParam = 0
     ret.transmissionType = structs.CarParams.TransmissionType.automatic
     ret.radarUnavailable = True
     ret.enableDsu = False                  # driving support unit
