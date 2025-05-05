3fe47ffba79c9dc8a1fbc894b163a67175c6cb3f
vmcy <mahcyen@gmail.com>
2025-05-05 15:42:52 +0800

    removed toyota default STEER DELTA UP DOWN LIMIT

--- opendbc/car/toyota/values.py
+++ opendbc/car/toyota/values.py
@@ -53,14 +53,6 @@ class CarControllerParams:
       self.ACCEL_MAX = 1.5  # m/s2, lower than allowed 2.0 m/s^2 for tuning reasons
     self.ACCEL_MIN = -3.5  # m/s2
 
-    if CP.lateralTuning.which() == 'torque':
-      self.STEER_DELTA_UP = 15       # 1.0s time to peak torque
-      self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
-    else:
-      self.STEER_DELTA_UP = 10       # 1.5s time to peak torque
-      self.STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
-
-
 class ToyotaSafetyFlags(IntFlag):
   # first byte is for EPS scaling factor
   ALT_BRAKE = (1 << 8)
