8661f32f1f2f3b842354e801755fc7df7222e55d
vmcy <mahcyen@gmail.com>
2025-05-09 17:15:17 +0800

    only allowing ACC_BRAKE

--- opendbc/safety/safety/safety_perodua.h
+++ opendbc/safety/safety/safety_perodua.h
@@ -20,7 +20,8 @@ static bool perodua_tx_hook(const CANPacket_t *to_send) {
 
   // STEERING_LKAS, ACC_BRAKE, ACC_CMD_HUD
   // TO ADD: 0x274 LKAS_HUD
-  if (addr == 0x1D0 || addr == 0x271 || addr == 0x273) {
+  //if (addr == 0x1D0 || addr == 0x271 || addr == 0x273) {
+    if (addr == 0x1D0 || addr == 0x271) {
   //if (addr == 0x1D0) {
     return true;
   }
@@ -33,7 +34,8 @@ static bool perodua_fwd_hook(int bus_num, int addr) {
 
   if (bus_num == 2) {
     // 0x1D0 is STEERING_LKAS, 0x271 is ACC_BRAKE, 0x273 is ACC_CMD_HUD
-    block_msg = ((addr == 0x1D0) || (addr == 0x271) || (addr == 0x273));
+    //block_msg = ((addr == 0x1D0) || (addr == 0x271) || (addr == 0x273));
+    block_msg = ((addr == 0x1D0) || (addr == 0x271));
     //block_msg = ((addr == 0x1D0));
   }
 
@@ -55,7 +57,7 @@ static safety_config perodua_init(uint16_t param) {
   static const CanMsg PERODUA_TX_MSGS[] = {
     {0x1D0, 0, 8, false},  // STEERING_LKAS
     {0x271, 0, 8, false},  // ACC_BRAKE
-    {0x273, 0, 8, false},  // ACC_CMD_HUD
+    //{0x273, 0, 8, false},  // ACC_CMD_HUD
   };
 
   safety_config ret = BUILD_SAFETY_CFG(perodua_rx_checks, PERODUA_TX_MSGS);
