e8d1fb15d7901cb46919cf9fc0a6e0060f7ffc12
vmcy <mahcyen@gmail.com>
2025-09-09 21:25:22 +0800

    try safety from ka2

--- opendbc/safety/safety/safety_perodua.h
+++ opendbc/safety/safety/safety_perodua.h
@@ -2,32 +2,51 @@
 
 #include "safety_declarations.h"
 
+const CanMsg DNGA_TX_MSGS[] = {{464, 0, 8, false}, {628, 0, 8, false}, {625, 0, 8, false}, {627, 0, 8, false}};
+RxCheck dnga_rx_checks[] = {
+  //{.msg = {{0x35F, 0, 8, .frequency = 20U}, { 0 }, { 0 }}},
+};
+
 // === RX HOOK ===
 // This version just always allows controls
 static void perodua_rx_hook(const CANPacket_t *to_push) {
-  UNUSED(to_push);
-
-  //int addr = GET_ADDR(to_push);
-
+  vehicle_moving = true;
   controls_allowed = true;
+  UNUSED(to_push);
 }
 
 // === TX HOOK ===
 // Only allow LKAS steering command through
 static bool perodua_tx_hook(const CANPacket_t *to_send) {
+  bool tx = true;
   int addr = GET_ADDR(to_send);
-  //return false;
+  int len = GET_LEN(to_send);
+  UNUSED(addr);
+  UNUSED(len);
 
-  // STEERING_LKAS, ACC_BRAKE, ACC_CMD_HUD
-  // TO ADD: 0x274 LKAS_HUD
-  //if (addr == 0x1D0 || addr == 0x271 || addr == 0x273) {
-  //if (addr == 0x1D0 || addr == 0x271) {
-  if (addr == 0x1D0) {
-    return true;
+  return tx;
+}
+
+/*
+static int perodua_fwd_hook(int bus_num, int addr) {
+  int bus_fwd = -1;
+
+  if (bus_num == 0) {
+    bus_fwd = 2;
   }
 
-  return false;
+  if (bus_num == 2) {
+    bool is_lkas_msg = ((addr == 464) || (addr == 628));
+    bool is_acc_msg = ((addr == 625) || (addr == 627));
+    bool block_msg = is_lkas_msg || is_acc_msg;
+    if (!block_msg) {
+      bus_fwd = 0;
+    }
+  }
+
+  return bus_fwd;
 }
+*/
 
 static bool perodua_fwd_hook(int bus_num, int addr) {
   bool block_msg = false;
@@ -36,32 +55,19 @@ static bool perodua_fwd_hook(int bus_num, int addr) {
     // 0x1D0 is STEERING_LKAS, 0x271 is ACC_BRAKE, 0x273 is ACC_CMD_HUD
     //block_msg = ((addr == 0x1D0) || (addr == 0x271) || (addr == 0x273));
     //block_msg = ((addr == 0x1D0) || (addr == 0x271));
-    block_msg = ((addr == 0x1D0));
+    //block_msg = ((addr == 0x1D0));
+
+    bool is_lkas_msg = ((addr == 464) || (addr == 628));
+    bool is_acc_msg = ((addr == 625) || (addr == 627));
+    block_msg = is_lkas_msg || is_acc_msg;
   }
 
   return block_msg;
 }
 
-// === INIT ===
-// No checks yet, just register hooks to satisfy Panda
 static safety_config perodua_init(uint16_t param) {
   UNUSED(param);
-
-  static RxCheck perodua_rx_checks[] = {
-    {.msg = {{0x1A0, 0, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 56U}, { 0 }, { 0 }}},  // WHEEL_SPEED
-    //{.msg = {{0x208, 0, 6, .ignore_checksum = true, .ignore_counter = true, .frequency = 31U}, { 0 }, { 0 }}},  // PCM_BUTTONS
-    {.msg = {{0x273, 2, 8, .ignore_checksum = true, .ignore_counter = true, .frequency = 20U}, { 0 }, { 0 }}},  // ACC_CMD_HUD
-  };
-
-  // TO ADD: 0x274 LKAS_HUD
-  static const CanMsg PERODUA_TX_MSGS[] = {
-    {0x1D0, 0, 8, false},  // STEERING_LKAS
-    //{0x271, 0, 8, false},  // ACC_BRAKE
-    //{0x273, 0, 8, false},  // ACC_CMD_HUD
-  };
-
-  safety_config ret = BUILD_SAFETY_CFG(perodua_rx_checks, PERODUA_TX_MSGS);
-  return ret;
+  return BUILD_SAFETY_CFG(dnga_rx_checks, DNGA_TX_MSGS);
 }
 
 // === REGISTER ===
